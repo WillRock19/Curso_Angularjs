1b76da5ab902621ac57689c1b4506fd1e6fdc99b
William <william.pinto@lambda3.com.br>
2020-10-31 10:12:28 -0300

    Baixando fotos do servidor usando a promise e o serviço

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,19 +1,14 @@
-angular.module("alurapic").controller("FotosController", function ($scope) {
-  $scope.fotos = [
-    {
-      titulo: "A Torre Negra",
-      url:
-        "https://i.pinimg.com/originals/42/0f/ea/420fea7da36d5923f6d993abbb44b05e.jpg",
-    },
-    {
-      titulo: "Randall Flagg",
-      url:
-        "https://www.stephenking.com.br/wp-content/uploads/2012/02/Randall-Flagg01.jpg",
-    },
-    {
-      titulo: "O Pistoleiro",
-      url:
-        "https://leitorcabuloso.com.br/wp-content/uploads/2017/05/torre-negra-destaque.jpg",
-    },
-  ];
-});
+angular
+  .module("alurapic")
+  .controller("FotosController", function ($scope, $http) {
+    $scope.fotos = [];
+
+    const promise = $http.get("v1/fotos");
+    promise
+      .then(function (retorno) {
+        $scope.fotos = retorno.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
