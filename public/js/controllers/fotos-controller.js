ecb4087be3e274c090f1c21898053fb7d28420fc
William <william.pinto@lambda3.com.br>
2020-10-31 10:06:09 -0300

    Adicionadas mais fotos e usando a diretiva ng-repeat para criar um for dentro do HTML

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,7 +1,19 @@
 angular.module("alurapic").controller("FotosController", function ($scope) {
-  $scope.foto = {
-    titulo: "the-torre-negra",
-    url:
-      "https://i.pinimg.com/originals/42/0f/ea/420fea7da36d5923f6d993abbb44b05e.jpg",
-  };
+  $scope.fotos = [
+    {
+      titulo: "A Torre Negra",
+      url:
+        "https://i.pinimg.com/originals/42/0f/ea/420fea7da36d5923f6d993abbb44b05e.jpg",
+    },
+    {
+      titulo: "Randall Flagg",
+      url:
+        "https://www.stephenking.com.br/wp-content/uploads/2012/02/Randall-Flagg01.jpg",
+    },
+    {
+      titulo: "O Pistoleiro",
+      url:
+        "https://leitorcabuloso.com.br/wp-content/uploads/2017/05/torre-negra-destaque.jpg",
+    },
+  ];
 });
