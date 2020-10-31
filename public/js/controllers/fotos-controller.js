2ce90e3d82761ac79be49c0f64dd870a36495612
William <william.pinto@lambda3.com.br>
2020-10-31 09:55:58 -0300

    AULA 02_INICIO

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,6 +1,6 @@
 angular.module("alurapic").controller("FotosController", function ($scope) {
   $scope.foto = {
-    titulo: "torre-negra",
+    titulo: "the-torre-negra",
     url:
       "https://i.pinimg.com/originals/42/0f/ea/420fea7da36d5923f6d993abbb44b05e.jpg",
   };
